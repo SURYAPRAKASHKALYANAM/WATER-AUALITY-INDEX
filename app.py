c75f47b0bd47fd391a61d479183e2e331dc984df
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2023-05-19 22:42:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask,request,render_template,jsonify
+import pickle
+app=Flask(__name__)
+model=pickle.load(open("nik_model.pkl","rb"))
+@app.route('/')
+def home():
+    return render_template("index.html")
+@app.route('/predict',methods=['POST'])
+def predict():
+    print(request.form.values())
+    features=[float(x) for x in request.form.values()]
+    final_features=list(np.array(features).reshape(1,-1))
+    prediction=model.predict(final_features)
+    output=round(prediction[0],4)
+    return render_template("index.html",prediction_text="WQI IS {}".format(output))
+if __name__=="__main__":
+    app.run(debug=True)
